be62755a6b155579bf325887873c14cb7b62b8b4
CaptainFest <xanilegendx@gmail.com>
2019-05-17 21:45:11 +0300

    making_train

--- my_main.py
+++ my_main.py
@@ -3,6 +3,7 @@ import torch.nn
 import time
 import argparse
 import sklearn.metrics as metrics
+from ignite.metrics import Precision, Recall
 
 import torch
 import torchvision
@@ -129,7 +130,8 @@ def main():
                 # state = load_weights(model_id)
                 # model.load_state_dict(state['model'])
                 train_metrics, valid_metrics = 0, 0
-
+                prec = Precision()
+                rec = Recall()
                 ##################################### training #############################################
                 n1 = len(train_loader)
                 for i, (train_image_batch, train_labels_batch) in enumerate(train_loader):
@@ -143,7 +145,6 @@ def main():
 
                     output_probs = models_pool(train_image_batch)  # models_pool[model_id](train_image_batch)
 
-
                     loss = nn.BCEWithLogitsLoss()
                     loss = loss(output_probs, train_labels_batch)
                     #nn.BCEWithLogitsLoss(output_probs, train_labels_batch)
@@ -155,8 +156,9 @@ def main():
                     # print(output_probs)
                     outputs = (output_probs > 0.5)
                     # print(outputs)
-
-                    train_labels_batch = train_labels_batch.cpu().detach().numpy()
+                    prec.update((outputs, train_labels_batch))
+                    rec.update((outputs, train_labels_batch))
+                    """train_labels_batch = train_labels_batch.cpu().detach().numpy()
                     outputs = outputs.cpu().detach().numpy()
 
                     epoch_time = time.time() - start_time
@@ -165,13 +167,17 @@ def main():
                     train_metrics = {'precision': precision,
                                      'recall': recall,
                                      # 'F1_score': (precision * recall * 2 / (precision + recall)).mean(),
-                                     'epoch_time': epoch_time}
-                    if i%20 == 0:
-                        print(train_metrics)
-
+                                     'epoch_time': epoch_time}"""
+                train_metrics = {'precision': prec.compute(),
+                                 'recall': rec.compute()}
+                print(train_metrics)
+                prec.reset()
+                rec.reset()
                 ##################################### validation ###########################################
                 with torch.no_grad():
                     n2 = len(valid_loader)
+                    prec = Precision()
+                    rec = Recall()
                     for i, (valid_image_batch, valid_labels_batch) in enumerate(valid_loader):
                         print(f'\rBatch {i} / {n2}', end='')
                         valid_image_batch = valid_image_batch.permute(0, 3, 1, 2).to(device).type(torch.cuda.FloatTensor)
@@ -184,16 +190,22 @@ def main():
                         loss = nn.BCEWithLogitsLoss()
                         loss = loss(output_probs, valid_labels_batch)
 
-                        valid_labels_batch = valid_labels_batch.cpu().detach().numpy()
+                        prec.update((outputs, train_labels_batch))
+                        rec.update((outputs, train_labels_batch))
+
+                        """valid_labels_batch = valid_labels_batch.cpu().detach().numpy()
                         outputs = outputs.cpu().detach().numpy()
 
                         precision = metrics.precision_score(valid_labels_batch, outputs, average='samples')
-                        recall = metrics.recall_score(valid_labels_batch, outputs, average='samples')
+                        recall = metrics.recall_score(valid_labels_batch, outputs, average='samples')"""
 
-                        valid_metrics = {'precision': precision,
-                                         'recall': recall}
                                          # 'F1_score': (precision * recall * 2 / (precision + recall)).mean()}
-                        print(valid_metrics)
+
+                valid_metrics = {'precision': prec.compute(),
+                                 'recall': rec.compute()}
+                print(valid_metrics)
+                prec.reset()
+                rec.reset()
 
                 # write events
                 # write_event(log, step, epoch=epoch, train_metrics=train_metrics, valid_metrics=valid_metrics)
