6d272c1d8af63749bed7ccffa0df790d82bcabe5
CaptainFest <xanilegendx@gmail.com>
2019-05-23 19:59:44 +0300

    making_train

--- my_dataset.py
+++ my_dataset.py
@@ -1,33 +1,35 @@
 import os
-
 import torch
 import torchvision.transforms.functional as TF
 from keras.preprocessing.image import array_to_img, img_to_array
 from torch.utils.data import Dataset, DataLoader
 from utils import load_image
-import pandas as pd
 import random
 import numpy as np
+import math
 
 
 class MyDataset(Dataset):
 
-    def __init__(self, train_test_id, image_path, train, pretrained, augment_list, act):
+    def __init__(self, train_test_id, labels_ids, args, train, ids):
 
         self.train_test_id = train_test_id
-        self.image_path = image_path
+        self.image_path = args.image_path
+        self.pretrained = args.pretrained
+        self.augment_list = args.augment_list
+        self.labels_ids = labels_ids
         self.train = train
-        self.pretrained = pretrained
-        self.augment_list = augment_list
-        self.mask_ind = pd.read_csv('mask_ind.csv')
-        self.act = act
-
-        if self.train:
-            self.mask_ind = self.mask_ind[self.train_test_id['Split'] == 'train'].values.astype('uint8')
-            self.train_test_id = self.train_test_id[self.train_test_id['Split'] == 'train'].ID.values
-            print('Train =', self.train, 'train_test_id.shape: ', self.train_test_id.shape)
+        if train:
+            if ids.size != 0:
+                self.labels_ids = self.labels_ids.iloc[ids, :][self.train_test_id['Split'] == 'train'].values.astype('uint8')
+                self.train_test_id = self.train_test_id.iloc[ids, :][self.train_test_id['Split'] == 'train'].ID.values
+                print('Train =', self.train, 'train_test_id.shape: ', self.train_test_id.shape)
+            else:
+                self.labels_ids = self.labels_ids[self.train_test_id['Split'] == 'train'].values.astype('uint8')
+                self.train_test_id = self.train_test_id[self.train_test_id['Split'] == 'train'].ID.values
+                print('Train =', self.train, 'train_test_id.shape: ', self.train_test_id.shape)
         else:
-            self.mask_ind = self.mask_ind[self.train_test_id['Split'] != 'train'].values.astype('uint8')
+            self.labels_ids = self.labels_ids[self.train_test_id['Split'] != 'train'].values.astype('uint8')
             self.train_test_id = self.train_test_id[self.train_test_id['Split'] != 'train'].ID.values
             print('Train =', self.train, 'train_test_id.shape: ', self.train_test_id.shape)
         self.n = self.train_test_id.shape[0]
@@ -96,22 +98,73 @@ class MyDataset(Dataset):
                 image, mask = self.transform_fn(image, mask)
 
         image_with_mask = np.dstack((image, mask))
-
-        labels = self.mask_ind[index, :]
+        labels = self.labels_ids[index, :]
 
         return image_with_mask, labels, name
 
 
-def make_loader(train_test_id, image_path, args, train=True, shuffle=True):
-    data_set = MyDataset(train_test_id=train_test_id,
-                         image_path=image_path,
-                         train=train,
-                         pretrained=args.pretrained,
-                         augment_list=args.augment_list)
+class ActiveDataset(Dataset):
+    def __init__(self, train_test_id, args, ids):
+        self.train_test_id = train_test_id
+        self.image_path = args.image_path
+        self.train_test_id = self.train_test_id.iloc[ids, :][self.train_test_id['Split'] == 'train'].ID.values
+        print('Active_phase ','train_test_id.shape: ', self.train_test_id.shape)
+        self.n = self.train_test_id.shape[0]
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, index):
+        name = self.train_test_id[index]
+        path = self.image_path
+        image = load_image(os.path.join(path, '%s.h5' % (name)), 'image')
+        return image, name
+
+
+def make_loader(train_test_id, labels_ids, args, ids, batch_size, train=True, active_phase=False, shuffle=True):
+
+    if not active_phase:
+        data_set = MyDataset(train_test_id=train_test_id,
+                             labels_ids=labels_ids,
+                             args=args,
+                             train=train,
+                             ids=ids)
+    else:
+        data_set = ActiveDataset(train_test_id=train_test_id,
+                                 args=args,
+                                 ids=ids)
     data_loader = DataLoader(data_set,
-                             batch_size=args.batch_size,
+                             batch_size=batch_size,
                              shuffle=shuffle,
                              num_workers=args.workers,
                              pin_memory=torch.cuda.is_available()
                              )
     return data_loader
+
+
+def cos_similarity(v1, v2):
+    "compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)"
+    sumxx, sumxy, sumyy = 0, 0, 0
+    for i in range(len(v1)):
+        x = v1[i]
+        y = v2[i]
+        sumxx += x * x
+        sumyy += y * y
+        sumxy += x * y
+    return sumxy / math.sqrt(sumxx * sumyy)
+
+
+def calculate_similarities(train_test_id, mask_ind, args, non_annotated, most_uncertain):
+    dl_non_annotated = make_loader(train_test_id, mask_ind, args, non_annotated, batch_size=1, train=True,
+                                   active_phase=True, shuffle=False)
+    dl_candidates = make_loader(train_test_id, mask_ind, args, most_uncertain, batch_size=1, train=True,
+                                active_phase=True, shuffle=False)
+    cos_sim_table = np.empty((len(dl_candidates), len(dl_non_annotated)))
+    for i, (image2_tensor, _) in enumerate(dl_non_annotated):
+        non_annotated_image_np = image2_tensor.cpu().numpy().ravel()
+        print(i)
+        for k, (image_tensor, _) in enumerate(dl_candidates):
+            candidate_image_np = image_tensor.cpu().numpy().ravel()
+            cos_sim_table[k, i] = cos_similarity(candidate_image_np, non_annotated_image_np)
+    return cos_sim_table
+
