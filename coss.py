60e8ff0584217ec0b05b1222648eed41641e8dfd
CaptainFest <xanilegendx@gmail.com>
2019-05-24 00:20:39 +0300

    making_train

--- coss.py
+++ coss.py
@@ -1,7 +1,23 @@
 from my_dataset import calculate_similarities
+import pandas as pd
+import argparse
+import numpy as np
 
-def create_sim_table_file():
+def create_sim_table_file(train_test_id, mask_ind, args, non_annotated, most_uncertain):
 
-    table = calculate_similarities()
+    table = calculate_similarities(train_test_id, mask_ind, args, non_annotated, most_uncertain)
+    np.save('similarities_table', table)
 
-create_sim_table_file()
+
+train_test_id = pd.read_csv('train_test_id.csv')
+mask_ind = pd.read_csv('mask_ind.csv')
+parser = argparse.ArgumentParser()
+arg = parser.add_argument
+arg('--batch-size', type=int, default=1)
+arg('--image-path', type=str, default='/home/irek/My_work/train/h5_112/')
+arg('--mask-path', type=str, default='/home/irek/My_work/train/binary/')
+arg('--mode', type=str, default='simple', choices=['simple', 'classic_AL', 'grid_AL'])
+args = parser.parse_args()
+non_annotated = train_test_id[train_test_id['Split'] == 'train'].index.tolist()
+most_uncertain = non_annotated
+create_sim_table_file(train_test_id, mask_ind, args, non_annotated, most_uncertain)
