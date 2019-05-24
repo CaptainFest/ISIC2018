7700e2ddf005ffd3771215f4c304d9d64011fffd
CaptainFest <xanilegendx@gmail.com>
2019-05-24 19:00:52 +0300

    making_train

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import re
 
 import numpy as np
 import h5py
-import torch
+import torch, torchvision
 import pandas as pd
 import os
 import sys
@@ -22,34 +22,34 @@ def load_image(file_name, type='image'):
     return file_np
 
 
-def save_weights(model, model_id, model_path, ep, step, train_metrics, valid_metrics):
-    torch.save({'model': model.state_dict(), 'model_id':model_id, 'epoch': ep, 'step': step, 'valid_loss': valid_metrics['loss1'], 'train_loss': train_metrics['loss1']},
+def write_tensorboard(writer, epoch, train_metrics, valid_metrics):
+
+    valid_loss = valid_metrics['loss']
+    train_loss = train_metrics['loss']
+    train_precision, valid_precision = train_metrics['precision'], valid_metrics['precision']
+    train_recall, valid_recall = train_metrics['recall'],valid_metrics['recall']
+    train_f1_score, valid_f1_score = train_metrics['f1_score'], valid_metrics['f1_score']
+    writer.add_scalars('loss', {'train': train_loss, 'valid': valid_loss}, epoch)
+    writer.add_scalars('precision', {'train': train_precision, 'valid': valid_precision}, epoch)
+    writer.add_scalars('recall', {'train': train_recall, 'valid': valid_recall}, epoch)
+    writer.add_scalars('f1_score', {'train': train_f1_score, 'valid': valid_f1_score}, epoch)
+
+
+def save_weights(model, model_path, ep, train_metrics, valid_metrics):
+    torch.save({'model': model.state_dict(), 'epoch_time': ep, 'valid_loss': valid_metrics['loss1'], 'train_loss': train_metrics['loss1']},
                str(model_path)
                )
 
-def write_event(log, step, epoch, train_metrics, valid_metrics):
-    CMD = 'epoch:{} step:{} time:{:.2f} \n train_loss:{:.3f} {:.3f} {:.3f} {:.3f} train_auc1:{} {} {} {} {} train_auc2:{} {} {} {} {} train_jaccard:{:.3f} {:.3f} {:.3f} {:.3f} {:.3f} {:.3f} \n valid_loss:{:.3f} {:.3f} {:.3f} {:.3f} valid_auc1:{} {} {} {} {} valid_auc2:{} {} {} {} {} valid_jaccard:{:.3f} {:.3f} {:.3f} {:.3f} {:.3f} {:.3f}'.format(
-        epoch, step, train_metrics['epoch_time'],
-        train_metrics['loss'],train_metrics['loss1'],train_metrics['loss2'],train_metrics['loss3'],
-        train_metrics['jaccard'],train_metrics['jaccard1'],train_metrics['jaccard2'],train_metrics['jaccard3'],train_metrics['jaccard4'],train_metrics['jaccard5'],
-        valid_metrics['loss'], valid_metrics['loss1'], valid_metrics['loss2'], valid_metrics['loss3'],
-        valid_metrics['jaccard'],valid_metrics['jaccard1'],valid_metrics['jaccard2'],valid_metrics['jaccard3'],valid_metrics['jaccard4'],valid_metrics['jaccard5'],
+
+def write_event(log, train_metrics, valid_metrics):
+    CMD='epoch:{} time:{:.2f} train_loss:{:.4f} train_precision:{:.3f} train_recall:{:.3f} train_f1_score:{:.3f}' \
+        'valid_loss:{:.4f} valid_precision:{:.3f} valid_recall: {:.3f} valid_f1_score:{:.3f}'.\
+        format(train_metrics['epoch'], train_metrics['epoch_time'], train_metrics['loss'], train_metrics['precision'],
+               train_metrics['recall'], train_metrics['f1_score'],
+               valid_metrics['loss'], valid_metrics['precision'], valid_metrics['recall'], valid_metrics['f1_score']
     )
     print(CMD)
     log.write(json.dumps(CMD))
-    # log.write(json.dumps(valid_metrics, sort_keys=True))
-    log.write('\n')
-    log.flush()
-
-
-def write_valid_event(log, valid_metrics):
-    CMD = 'valid_loss:{:.3f} {:.3f} {:.3f} {:.3f} valid_auc1:{} {} {} {} {} valid_auc2:{} {} {} {} {} valid_jaccard:{:.3f} {:.3f} {:.3f} {:.3f} {:.3f} {:.3f}'.format(
-        valid_metrics['loss'], valid_metrics['loss1'], valid_metrics['loss2'], valid_metrics['loss3'],
-        valid_metrics['jaccard'],valid_metrics['jaccard1'],valid_metrics['jaccard2'],valid_metrics['jaccard3'],valid_metrics['jaccard4'],valid_metrics['jaccard5'],
-    )
-    print(CMD)
-    log.write(json.dumps(CMD))
-    # log.write(json.dumps(valid_metrics, sort_keys=True))
     log.write('\n')
     log.flush()
 
@@ -78,22 +78,10 @@ def set_train_layers(model, train_layer_names=None):
                 para.requires_grad = False
 
 
-def get_freeze_layer_names(model,part):
-    freeze_layers = []
-    for ind, (name, para) in enumerate(model.named_parameters()):
-        if re.search(part, name):
-            #print(ind, name, para.numel(), para.requires_grad)
-            freeze_layers.append(name)
-        if part == 'encoder':
-            if re.search('center', name):
-                freeze_layers.append(name)
-    return freeze_layers
-
-
 class ResultAndArgsSaver:
 
     def __init__(self, args):
-        self.data = pd.DataFrame(columns=['epoch', 'time', 'loss', 'precision', 'recall', 'f1_score'])
+        self.data = pd.DataFrame(columns=['epoch', 'epoch_time', 'loss', 'precision', 'recall', 'f1_score'])
         self.model_name = args.model
 
     def update(self, results):
